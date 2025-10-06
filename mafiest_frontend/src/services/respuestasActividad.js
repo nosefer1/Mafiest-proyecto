7157e845d37c32b3c9fb1884f775c58bfb2551c2
mafiest-final-tech <slisnpe@gmail.com>
2025-10-05 21:09:02 -0500

    Update respuestasActividad.js

--- mafiest_frontend/src/services/respuestasActividad.js
+++ mafiest_frontend/src/services/respuestasActividad.js
@@ -4,26 +4,45 @@ import { BASE_URL } from './config'
 const baseUrl = `${BASE_URL}/respuestas-actividad`
 
 let token = null
-export const setToken = newToken => { token = `Bearer ${newToken}` }
 
-export const subirRespuestaArchivo = async (actividadId, archivo, archivoUrl) => {
+const setToken = newToken => {
+  token = `Bearer ${newToken}`
+}
+
+const subirRespuestaArchivo = async (actividadId, archivo, archivoUrl) => {
   const config = { headers: { Authorization: token } }
   const formData = new FormData()
   if (archivo) formData.append('archivo', archivo)
   if (archivoUrl) formData.append('archivoUrl', archivoUrl)
-  return (await axios.post(`${baseUrl}/actividad/${actividadId}`, formData, config)).data
+  const response = await axios.post(`${baseUrl}/actividad/${actividadId}`, formData, config)
+  return response.data
 }
 
-export const eliminarRespuesta = async (respuestaId) => {
+const eliminarRespuesta = async (respuestaId) => {
   const config = { headers: { Authorization: token } }
-  return (await axios.delete(`${baseUrl}/${respuestaId}`, config)).data
+  const response = await axios.delete(`${baseUrl}/${respuestaId}`, config)
+  return response.data
 }
 
-export const obtenerRespuestasActividad = async (actividadId) => {
-  return (await axios.get(`${baseUrl}/actividad/${actividadId}`)).data
+const obtenerRespuestasActividad = async (actividadId) => {
+  const response = await axios.get(`${baseUrl}/actividad/${actividadId}`)
+  return response.data
 }
 
-export const setRetroalimentacion = async (respuestaId, retroalimentacion, nota) => {
+const setRetroalimentacion = async (respuestaId, retroalimentacion, nota) => {
   const config = { headers: { Authorization: token } }
-  return (await axios.put(`${baseUrl}/retroalimentacion/${respuestaId}`, { retroalimentacion, nota }, config)).data
+  const response = await axios.put(
+    `${baseUrl}/retroalimentacion/${respuestaId}`,
+    { retroalimentacion, nota },
+    config
+  )
+  return response.data
+}
+
+export default {
+  setToken,
+  subirRespuestaArchivo,
+  eliminarRespuesta,
+  obtenerRespuestasActividad,
+  setRetroalimentacion
 }
